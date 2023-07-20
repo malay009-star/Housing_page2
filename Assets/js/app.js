7b816e4f59af70cce8a7a23b1b56158a4ea2a50e
malay009-star <117511789+malay009-star@users.noreply.github.com>
2023-07-20 16:53:52 +0500

    we create a new page about house website.

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,6 +1,44 @@
-var home_tab = document.getElementById('home-tab');
-console.log(home_tab);
+let menu = document.getElementById('menu');
+let show_menu = document.querySelectorAll('.show-menu');
+let caard = document.querySelectorAll('.caard');
+let caard_body = document.querySelectorAll('.caard-body');
+console.log(show_menu);
+console.log(caard);
+console.log(menu);
 
-home_tab.addEventListener('click' , ()=>{
-    home_tab.background.style = 'red';
+menu.addEventListener('click' , ()=>{
+    // show_menu.classList.toggle('py-5');
+    for(i=0 ; i<caard.length ; i++)
+    {
+        show_menu[i].classList.toggle('d-flex');
+        show_menu[i].classList.toggle('ps-4');
+        caard[i].classList.toggle('w-100');
+        caard_body[i].classList.toggle('ps-5');
+        caard_body[i].classList.toggle('ms-5');
+    }
+ 
+    // show_menu[1].classList.toggle('d-flex');
+    // show_menu[1].classList.toggle('justify-content-evenly');
+    // caard[1].classList.toggle('w-100');  
+    // show_menu[2].classList.toggle('d-flex');
+    // show_menu[2].classList.toggle('justify-content-evenly');
+    // caard[2].classList.toggle('w-100');
+    // show_menu[2].classList.toggle('d-flex');
+    // show_menu[2].classList.toggle('justify-content-evenly');
+    // caard[2].classList.toggle('w-100');
+    // show_menu[2].classList.toggle('d-flex');
+    // show_menu[2].classList.toggle('justify-content-evenly');
+    // caard[2].classList.toggle('w-100');
+    // show_menu[2].classList.toggle('d-flex');
+    // show_menu[2].classList.toggle('justify-content-evenly');
+    // caard[2].classList.toggle('w-100');
+    // show_menu[2].classList.toggle('d-flex');
+    // show_menu[2].classList.toggle('justify-content-evenly');
+    // caard[2].classList.toggle('w-100');
+    // show_menu[2].classList.toggle('d-flex');
+    // show_menu[2].classList.toggle('justify-content-evenly');
+    // caard[2].classList.toggle('w-100');
+    // show_menu[2].classList.toggle('d-flex');
+    // show_menu[2].classList.toggle('justify-content-evenly');
+    // caard[2].classList.toggle('w-100');
 })
